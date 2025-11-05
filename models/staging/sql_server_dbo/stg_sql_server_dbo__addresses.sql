2b6656cb9c1b0cdfdf33697984373f4bbf675f04
Pablo Valenzuela <valenbetis@gmail.com>
2025-11-05 18:44:12 +0000

    stg addresses check

--- models/staging/sql_server_dbo/stg_sql_server_dbo__addresses.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__addresses.sql
@@ -10,13 +10,13 @@ with src_addresses as (
 ),
 renamed_casted AS (
     SELECT
-        address_id
-        , address
-        , zipcode
-        , state
-        , country
-        , _fivetran_deleted AS date_deleted
-        , _fivetran_synced AS date_load
+        address_id, 
+        address,
+        zipcode,
+        state,
+        country,
+        convert_timezone('UTC',_fivetran_synced) AS date_load_utc,
+        _fivetran_deleted AS date_deleted
     FROM src_addresses
     )
 

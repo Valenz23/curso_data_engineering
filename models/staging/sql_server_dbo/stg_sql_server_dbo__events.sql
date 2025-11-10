5256075306e9351aede9226a5db515e2a180563d
Pablo Valenzuela <valenbetis@gmail.com>
2025-11-10 11:18:50 +0000

    stg events added

--- models/staging/sql_server_dbo/stg_sql_server_dbo__events.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__events.sql
@@ -11,7 +11,16 @@ with src_events as (
 renamed_casted AS (
     SELECT
 
-        -- aaaaaaaaaaaaaaaaaa
+        event_id,
+        page_url,
+        event_type, -- sacar a otra tabla
+        user_id,
+        product_id,
+        session_id,
+        convert_timezone('UTC',created_at) as created_at,
+        order_id,
+        convert_timezone('UTC',_fivetran_synced) AS date_load_utc,
+        _fivetran_deleted AS date_deleted
         
     FROM src_events
     )

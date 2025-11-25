06cb375f13b94791b8c8acc949c41facc2cdc643
Pablo Valenzuela <valenbetis@gmail.com>
2025-11-25 11:56:48 +0000

    int model to calculate playoff rounds and nascar cup champion

---
+++ models/marts/core/intermediate/int_champ_4__winner.sql
@@ -0,0 +1,20 @@
+with 
+dr as (select * from {{ref("dim_driver")}}),
+ra as (select * from {{ref("dim_race")}}),
+c4 as (select * from {{ref("int_champ_4__classification")}}),
+
+winner as (
+    select 
+        c4.season as season,
+        c4.driver as driver,
+        rp.final_pos as final_pos
+    from {{ref("stg_nascar__race_perf")}} rp
+    join dr on dr.driver_id = rp.driver_id
+    join ra on ra.race_id=rp.race_id
+    join c4 on c4.driver = dr.driver_name
+    where ra.race_number = 36
+    order by final_pos
+
+)
+
+select * from winner
---
+++ models/marts/core/intermediate/int_round_12__classification.sql
@@ -0,0 +1,23 @@
+with 
+dr as (select * from {{ref("dim_driver")}}),
+ra as (select * from {{ref("dim_race")}}),
+r16 as (select * from {{ref("int_round_16__classification")}}),
+
+r12 as (
+    select top 12
+        r16.season as season,
+        r16.driver as driver,
+        count(case when rp.final_pos=1 then 1 end) as victories,
+        sum(rp.total_points) as total_points
+    from {{ref("stg_nascar__race_perf")}} rp
+    join dr on dr.driver_id = rp.driver_id
+    join ra on ra.race_id=rp.race_id
+    join r16 on r16.driver = dr.driver_name
+    where ra.race_number between 27 and 29
+    group by driver, r16.season
+    order by victories desc, total_points desc
+
+
+)
+
+select * from r12
